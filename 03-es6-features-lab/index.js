dad19ee58c761429038d8afe58ceb502525a87cb
Trayan Iliev <office@iproduct.org>
2021-10-31 12:01:14 +0200

    demos added

--- 03-es6-features-lab/index.js
+++ 03-es6-features-lab/index.js
@@ -1,33 +1,32 @@
-function init() {
-    var resultsDiv = document.getElementById("results")
-    fetch("/users.json")
-        .then(resp => resp.json())
-        .then(usersArr => {
-            console.log(usersArr);
-            return Promise.all(
-                usersArr.map(user => fetch(`https://api.github.com/users/${user.name}`)
-                    .then(gitResponse => gitResponse.json())
-                )
-            )
-        })
-        .then(gitUsers => {
-            console.log(gitUsers);
-            return gitUsers.map(gitUser => {
-                var img = document.createElement("img")
-                resultsDiv.appendChild(img);
-                img.src = gitUser.avatar_url;
-                return img;
-            });
-        }).then(images => {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    images.forEach(img => {
-                        img.remove();
-                    });
-                    resolve('Demo finished.');
-                }, 10000);
-            });
-        }).then(result => {
-            console.log(result);
-        })
+async function init() {
+    try {
+        var resultsDiv = document.getElementById("results");
+        const resp = await fetch("http://localhost:3000/users");
+        const usersArr = await resp.json();
+        console.log(usersArr);
+        const gitUsers = await Promise.all(
+            usersArr.map(async user => {
+                const gitResponse = await fetch(`https://api.github.com/users/${user.name}`)
+                return gitResponse.json()
+            })
+        );
+        console.log(gitUsers);
+        const images = gitUsers.map(gitUser => {
+            var img = document.createElement("img")
+            resultsDiv.appendChild(img);
+            img.src = gitUser.avatar_url;
+            return img;
+        });
+        const result = await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                images.forEach(img => {
+                    img.remove();
+                });
+                resolve('Demo finished.');
+            }, 10000);
+        });
+        console.log(result);
+    } catch (err) {
+        console.log(`Error: ${err}`)
+    }
 }
