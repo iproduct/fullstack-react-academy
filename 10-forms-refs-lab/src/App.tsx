7e888d53021d23e2548d95c453925dc8ac0a03f2
Trayan Iliev <office@iproduct.org>
2021-11-28 11:35:49 +0200

    refs forms project demo added

---
+++ 10-forms-refs-lab/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { UncontrolledFormClass } from './UncontrolledFormClass';
+
+function App() {
+  return (
+    <div className="App-container">
+      <UncontrolledFormClass />
+    </div>
+  );
+}
+
+export default App;
