8167cf7b954cb28bfed081cfd51cc4ce4bc96a03
Trayan Iliev <office@iproduct.org>
2021-11-28 14:07:01 +0200

    basic procedural validation added to form

--- 10-forms-refs-lab/src/FormikBasicForm.tsx
+++ 10-forms-refs-lab/src/FormikBasicForm.tsx
@@ -4,20 +4,40 @@ import { MyForm } from './MyForm';
 
 
 export interface FormFields {
-    email: string
+    email: string;
+    password: string;
 }
+
+export interface FormErrors {
+    email?: string;
+    password?: string;
+}
+
+const validate = (values: FormFields) => {
+    const errors: FormErrors = {};
+
+    if (!values.email) {
+        errors.email = 'Email is required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
+    }
+
+    return errors;
+};
+
 
 export const FormikBasicForm = () => {
     return (
         <Formik<FormFields>
-        initialValues={{email: ""}} 
-        onSubmit={
-            async values => {
-                await new Promise(resolve => setTimeout(resolve, 500));
-                alert(JSON.stringify(values))
-            }
-        }>
-            {props => (<MyForm {...props}/>)}
+            initialValues={{ email: "", password: "" }}
+            validate={validate}
+            onSubmit={
+                async values => {
+                    await new Promise(resolve => setTimeout(resolve, 500));
+                    alert(JSON.stringify(values))
+                }
+            }>
+            {props => (<MyForm {...props} />)}
         </Formik>
     )
 }
--- 10-forms-refs-lab/src/MyForm.tsx
+++ 10-forms-refs-lab/src/MyForm.tsx
@@ -2,18 +2,29 @@ import { FormikProps } from 'formik'
 import React from 'react'
 import { FormFields } from './FormikBasicForm'
 
-
 export const MyForm:React.FC<FormikProps<FormFields>> = props => {
     return (
-        <form onSubmit={props.handleSubmit}>
+        <form onSubmit={props.handleSubmit} onReset={props.handleReset}>
             <input 
                 type="text"
                 onChange={props.handleChange}
                 onBlur={props.handleBlur}
                 value={props.values.email}
-                name="email" 
+                name="email"
+                placeholder="email"  
             />
+            {props.errors.email && <div className="feedback">{props.errors.email}</div>}
+            
+            <input 
+                type="text"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.password}
+                name="password" 
+                placeholder="password" 
+                />
             <button type="submit">Submit</button>
+            <button type="reset">Reset</button>
         </form>
     )
 }
