3573e035a2e33c286b2a59d59dc4710ac30274b9
Trayan Iliev <office@iproduct.org>
2021-12-04 12:37:12 +0200

    field level validation with Formik

--- 10-forms-refs-lab/src/MyForm.tsx
+++ 10-forms-refs-lab/src/MyForm.tsx
@@ -1,42 +1,26 @@
-import { FormikProps } from 'formik'
+import { Field, Form, FormikProps } from 'formik'
 import React from 'react'
-import { FormFields } from './FormikBasicForm'
+import { FormFields, validateEmail } from './FormikBasicForm'
+
+
 
 export const MyForm:React.FC<FormikProps<FormFields>> = props => {
+    const { errors, touched, isValidating } = props;
     return (
-        <form onSubmit={props.handleSubmit} onReset={props.handleReset}>
-            <input 
-                type="text"
-                onChange={props.handleChange}
-                onBlur={props.handleBlur}
-                value={props.values.email}
-                name="email"
-                placeholder="email"  
-            />
-            {props.errors.email && <div className="feedback">{props.errors.email}</div>}
+        <Form>
+            <Field name="email" placeholder="Email" validate={validateEmail} />
+            {touched.email && errors.email && <div className="feedback">{errors.email}</div>}
 
-            <input 
-                type="text"
-                onChange={props.handleChange}
-                onBlur={props.handleBlur}
-                value={props.values.username}
-                name="username"
-                placeholder="username"  
-            />
-            {props.errors.username && <div className="feedback">{props.errors.username}</div>}
-
-            <input 
-                type="text"
-                onChange={props.handleChange}
-                onBlur={props.handleBlur}
-                value={props.values.password}
-                name="password" 
-                placeholder="password" 
-                />
-            {props.errors.password && <div className="feedback">{props.errors.password}</div>}
+            <Field name="username" placeholder="Username" />
+            {touched.username && errors.username && <div className="feedback">{errors.username}</div>}
+            
+            <Field name="password" placeholder="Password" />
+            {touched.password && errors.password && <div className="feedback">{errors.password}</div>}
 
             <button type="submit">Submit</button>
             <button type="reset">Reset</button>
-        </form>
+            <pre>Touched: {JSON.stringify(touched)}</pre>
+        </Form>
+
     )
 }
