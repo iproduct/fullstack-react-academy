52c6d4f55b5226d91910e0e8da23d9a577022a36
Trayan Iliev <office@iproduct.org>
2021-12-18 14:45:51 +0200

    post new POST

--- 15-tsnode-lab/src/01_dns.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var dns = require("dns");
-dns.resolve4('yahoo.com', function (err, addresses) {
-    console.log("yahoo.com IPs: ".concat(JSON.stringify(addresses)));
-    addresses.map(function (addr) { return dns.reverse(addr, function (err, hostnames) {
-        console.log("reverse for ".concat(addr, ": ").concat(JSON.stringify(hostnames)));
-    }); });
-});
--- 15-tsnode-lab/src/05_server-express.ts
+++ 15-tsnode-lab/src/05_server-express.ts
@@ -1,33 +1,56 @@
 import * as http from 'http';
+import { Server } from 'http';
 import { HttpError } from 'http-errors';
 import * as express from 'express';
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
+import MOCK_POSTS from './model/mock-posts';
+import { v4 as uuid4 } from 'uuid';
+import { Post, IdType } from './model/post';
+import { Http2Server } from 'http2';
 
-const hostname : string = process.env.HOST || '127.0.0.1'
+const hostname: string = process.env.HOST || '127.0.0.1'
 const port = parseInt(process.env.PORT || '9000')
 
 const app = express()
+let server: Server | undefined
 app.use(express.json())
 
-app.get('/', (req: Request, res: Response) =>{
-        res.set('Content-Type', 'application/json')
-        res.json()
+app.locals.db = new Map<IdType, Post>()
+
+MOCK_POSTS.map(post => {
+    post.id = uuid4();
+    return post;
+}).forEach(post => app.locals.db.set(post.id, post))
+
+app.get('/api/posts', (req: Request, res: Response) => {
+    res.set('Content-Type', 'application/json')
+    res.json(Array.from(app.locals.db.values()))
 });
 
-app.on('error', (e: HttpError )=> {
-    if(e.code === 'EADDRINUSE') {
+app.post('/api/posts', (req: Request, res: Response) => {
+    const newPost = req.body as Post
+    newPost.id = uuid4()
+    req.app.locals.db = req.app.locals.db.set(newPost.id, newPost)
+    res.set('Content-Type', 'application/json')
+    res.status(201).location(`/api/posts/${newPost.id}`).json(newPost)
+});
+
+
+server?.on('error', (e: HttpError) => {
+    if (e.code === 'EADDRINUSE') {
         setTimeout(() => {
             console.log(`Restarting the server: http://${hostname}:${port}`)
-            app.close()
+            server?.close()
             startServer()
         }, 3000)
     }
 })
 
 function startServer() {
-    app.listen(port, hostname, () => {
+    server = app.listen(port, hostname, () => {
         console.log(`Server running on: http://${hostname}:${port}`)
     })
 }
 
 startServer()
+
