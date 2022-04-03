ab3b285d755e7c96baa4b8af230b1b2a09848821
Trayan Iliev <office@iproduct.org>
2022-04-03 15:22:04 +0300

    socketio update

--- 16-chat-server-socketio/index.js
+++ 16-chat-server-socketio/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + 'public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 io.on('connection', function(socket) {
